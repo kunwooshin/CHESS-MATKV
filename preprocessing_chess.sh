c940ae7ff2e26d83921974628f2b0b962e85ff29
Kunwoo Shin <kunwooshin97@gmail.com>
2025-05-12 17:38:26 +0900

    Update preprocessing_chess.sh

--- preprocessing_chess.sh
+++ preprocessing_chess.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 CUDA_VISIBLE_DEVICES=0 torchrun --nproc_per_node 1 \
-   /home/kunwoo/CHESS-MATKV/preprocessing_chess.py \
+   $HOME/CHESS-MATKV/preprocessing_chess.py \
    --docs_dirs="$HOME/CHESS-MATKV/chess_pu/agent_template,$HOME/CHESS-MATKV/chess_pu/db_schema" \
    --cache_dirs="$HOME/CHESS-MATKV/chess_pu/agent_template/cache_8b,$HOME/CHESS-MATKV/chess_pu/db_schema/cache_8b" \
    --model_name "meta-llama/Llama-3.1-8B"
